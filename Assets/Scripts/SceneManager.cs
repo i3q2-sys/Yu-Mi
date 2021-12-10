4a7de06d6e49e46fde624c97687ee62de6087ec4
i3q2-sys <52488230+i3q2-sys@users.noreply.github.com>
2021-12-10 23:29:39 +0100

    SceneManager

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToMainScene() 
+    {
+
+    }
+
+    public void GoToMeditation() 
+    {
+
+    }
+
+    public void GoToHappyWall() 
+    {
+
+    }
+
+    public void GoToGame() 
+    {
+
+    }
+
+    public void Rant() 
+    {
+
+    }
+}
