edd5bacbf4ad596c3d5925d97c246ea4ef82ba1a
i3q2-sys <52488230+i3q2-sys@users.noreply.github.com>
2021-12-11 12:14:30 +0100

    orchestrator

---
+++ Assets/Scripts/Questions/Orchestrator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orchestrator : MonoBehaviour
+{
+    public void PlayPersonalityTest() 
+    {
+
+    }
+
+    public void PlayEmotionalTest() 
+    {
+
+    }
+
+    public void PlayMeditationStart() 
+    {
+
+    }
+
+    public void PlayMeditationEnd() 
+    {
+
+    }
+
+    public void PlayGameStart() 
+    {
+
+    }
+
+    public void PlayGameEnd() 
+    {
+
+    }
+
+    public void PlayHappyWall() 
+    {
+
+    }
+}
